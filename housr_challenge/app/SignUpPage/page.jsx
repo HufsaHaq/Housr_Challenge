bdeea44d2c46b9c6458be50bfac6892f04172475
Hufsa Haq <hufsahaq@gmail.com>
2025-11-09 00:35:54 +0000

    redirect to account page when login/sign up page done

--- housr_challenge/app/SignUpPage/page.jsx
+++ housr_challenge/app/SignUpPage/page.jsx
@@ -11,7 +11,7 @@ export default function LoginPage() {
 
   const handleLogin = (e) => {
     e.preventDefault();
-    alert(`Username: ${username}\nPassword: ${password}`);
+    window.location.href = "/account";
   };
 
   return (
